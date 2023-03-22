784895373583d5b7fa26f6298bc738a5a744afe3
DurandoAngiulo <87902832+DurandoAngiulo@users.noreply.github.com>
2023-03-22 13:37:09 -0400

    another 404 update

--- wp-content/themes/idm250-theme-01/404.php
+++ wp-content/themes/idm250-theme-01/404.php
@@ -10,7 +10,7 @@
     </div>
     <div class="mx-auto mt-5 flow-root max-w-lg sm:mt-7">
       <div class="flex justify-center">
-        <a href="/front-page.php" class="text-sm font-semibold">
+        <a href="https://durandoangiulo.com/idm250/" class="text-sm font-semibold">
           <span aria-hidden="true">&larr;</span>
           Return to Home
         </a>
